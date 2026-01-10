63d352a3e823e79664fe7047ad3d7dcb8e060735
Alisa Corporation <alisa.corporation@protonmail.com>
2026-01-10 12:34:51 +0200

    Update plugin to version 1.1.1 with IntelliJ Platform Plugin 2.x, Gradle 8.13, and compatibility fixes for IDEA 2024.3+

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,61 +1,65 @@
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "2.1.0"
-    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.intellij.platform") version "2.10.5"
 }
 
 group = "com.alisacorporation"
-version = "1.1.0"
+version = "1.1.1"
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("2021.1")
-    type.set("IC") // Target IDE Platform
-    plugins.set(listOf(/* Plugin Dependencies */))
+dependencies {
+    intellijPlatform {
+        intellijIdeaCommunity("2024.3")
+    }
 }
 
-tasks {
-    // Disable building searchable options since we don't have any
-    withType<org.jetbrains.intellij.tasks.BuildSearchableOptionsTask> {
-        enabled = false
-    }
-    
-    // Set the JVM compatibility versions
-    withType<JavaCompile> {
-        sourceCompatibility = JavaVersion.VERSION_11.toString()
-        targetCompatibility = JavaVersion.VERSION_11.toString()
-        options.encoding = "UTF-8"
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
-    }
+// Configure Gradle IntelliJ Platform Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
+intellijPlatform {
+    buildSearchableOptions = false
 
-    patchPluginXml {
-        version.set(project.version.toString())
-        sinceBuild.set("211") // Minimum IDE version
-        //untilBuild.set("252.*") // Maximum IDE version
-        changeNotes.set("""
+    pluginConfiguration {
+        version = project.version.toString()
+        ideaVersion {
+            sinceBuild = "243"
+            untilBuild = "999.*"
+        }
+        changeNotes = """
             <ul>
                 <li>Initial release: Copy file contents to clipboard from project view context menu</li>
             </ul>
-        """.trimIndent())
-        pluginDescription.set("""
+        """.trimIndent()
+        description = """
             Copy file content to clipboard directly from project view context menu. Maybe useful, to share with Claude AI, ChatGPT etc..
-        """.trimIndent())
+        """.trimIndent()
     }
 
-    signPlugin {
+    signing {
         certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
         privateKey.set(System.getenv("PRIVATE_KEY"))
         password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
     }
 
-    publishPlugin {
+    publishing {
         token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = JavaVersion.VERSION_21.toString()
+        targetCompatibility = JavaVersion.VERSION_21.toString()
+        options.encoding = "UTF-8"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "21"
     }
 }
